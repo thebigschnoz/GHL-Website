33c7f3f0cbad62096478b55100b5189935f2f64e
thebigschnoz <pendletongoodguy@gmail.com>
2024-12-13 22:35:23 -0500

    Started adding standings page and played with CSS

--- GHLWebsiteDjango/GHLWebsiteApp/urls.py
+++ GHLWebsiteDjango/GHLWebsiteApp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('standings/', views.standings, name="standings"),
+    path('team/<int:teamID>', views.team, name="team"),
+    path('player/<int:playerID>', views.player, name="player"),
 ]
--- GHLWebsiteDjango/GHLWebsiteApp/views.py
+++ GHLWebsiteDjango/GHLWebsiteApp/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "GHLWebsiteApp/index.html")
+
+def standings(request):
+    return render(request, "GHLWebsiteApp/standings.html")
+
+def team(request, teamID):
+    return render(request, "GHLWebsiteApp/team.html", {"teamID": teamID})
+
+def player(request, playerID):
+    return render(request, "GHLWebsiteApp/player.html", {"playerID": playerID})
