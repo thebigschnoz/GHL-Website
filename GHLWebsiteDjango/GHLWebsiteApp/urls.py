03cbfbc5b5c920b539b982d65eebcb141c5ad178
thebigschnoz <pendletongoodguy@gmail.com>
2024-12-14 15:53:55 -0500

    finished leaders and awards

--- GHLWebsiteDjango/GHLWebsiteApp/urls.py
+++ GHLWebsiteDjango/GHLWebsiteApp/urls.py
@@ -4,6 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('standings/', views.standings, name="standings"),
-    path('team/<int:teamID>', views.team, name="team"),
-    path('player/<int:playerID>', views.player, name="player"),
+    path('leaders/', views.leaders, name="leaders"),
+    path('skaters/', views.skaters, name="skaters"),
+    path('goalies/', views.goalies, name="goalies"),
+    path('team/<int:teamID>/', views.team, name="team"),
+    path('player/<int:playerID>/', views.player, name="player"),
+    path('awards/', views.awardsDef, name="awardsDef"),
+    path('awards/<int:award>/', views.awards, name="awards"),
+    path('draft/', views.draft, name="draft"),
 ]
--- GHLWebsiteDjango/GHLWebsiteApp/views.py
+++ GHLWebsiteDjango/GHLWebsiteApp/views.py
@@ -7,8 +7,26 @@ def index(request):
 def standings(request):
     return render(request, "GHLWebsiteApp/standings.html")
 
+def leaders(request):
+    return render(request, "GHLWebsiteApp/leaders.html")
+
+def skaters(request):
+    return render(request, "GHLWebsiteApp/skaters.html")
+
+def goalies(request):
+    return render(request, "GHLWebsiteApp/goalies.html")
+
 def team(request, teamID):
     return render(request, "GHLWebsiteApp/team.html", {"teamID": teamID})
 
 def player(request, playerID):
     return render(request, "GHLWebsiteApp/player.html", {"playerID": playerID})
+
+def draft(request):
+    return render(request, "GHLWebsiteApp/draft.html")
+
+def awardsDef(request):
+    return render(request, "GHLWebsiteApp/awards.html")
+
+def awards(request, award):
+    return render(request, "GHLWebsiteApp/awards.html", {"award": award})
