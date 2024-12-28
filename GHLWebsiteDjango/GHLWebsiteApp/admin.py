31e2bd7f001cf5748a3eb38ffc3ea952e1cd1795
thebigschnoz <pendletongoodguy@gmail.com>
2024-12-27 20:21:55 -0500

    admin views cleaning

--- GHLWebsiteDjango/GHLWebsiteApp/admin.py
+++ GHLWebsiteDjango/GHLWebsiteApp/admin.py
@@ -3,14 +3,25 @@ from django.contrib import admin
 from .models import Seasons, Games, AwardsList, TeamList, PlayerList, Positions, Builds, Awards, SkaterRecords, GoalieRecords, TeamRecords
 
 # Register your models here.
+class TeamListAdmin(admin.ModelAdmin):
+    list_display = ("club_full_name", "ea_club_num", "club_abbr", "team_logo_link")
 
-admin.site.register(Seasons)
+class BuildsAdmin(admin.ModelAdmin):
+    list_display = ("build", "buildShort")
+
+class PlayerListAdmin(admin.ModelAdmin):
+    list_display = ("username", "current_team")
+
+class SeasonsAdmin(admin.ModelAdmin):
+    list_display = ("season_text", "season_num", "isPlayoff")
+
+admin.site.register(Seasons, SeasonsAdmin)
 admin.site.register(Games)
 admin.site.register(AwardsList)
-admin.site.register(TeamList)
-admin.site.register(PlayerList)
+admin.site.register(TeamList, TeamListAdmin)
+admin.site.register(PlayerList, PlayerListAdmin)
 admin.site.register(Positions)
-admin.site.register(Builds)
+admin.site.register(Builds, BuildsAdmin)
 admin.site.register(Awards)
 admin.site.register(SkaterRecords)
 admin.site.register(GoalieRecords)
