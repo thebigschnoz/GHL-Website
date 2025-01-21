2361390466764ae07e2cdfdf161b03e29f654cd6
thebigschnoz <pendletongoodguy@gmail.com>
2025-01-20 21:54:00 -0500

    added celery beat to apps

--- GHLWebsiteDjango/GHLWebsiteDjango/settings.py
+++ GHLWebsiteDjango/GHLWebsiteDjango/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'corsheaders',
     'GHLWebsiteApp',
+    'django_celery_beat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
