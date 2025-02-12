00d4977838508aebf4bbc3a8b2522613f480295c
thebigschnoz <pendletongoodguy@gmail.com>
2025-02-11 22:45:19 -0500

    celery commands update

--- GHLWebsiteDjango/GHLWebsiteDjango/celery.py
+++ GHLWebsiteDjango/GHLWebsiteDjango/celery.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from ..GHLWebsiteApp.management.commands import fetch_and_process_games_task
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GHLWebsiteDjango.settings')
 
-app = Celery('GHLWebsiteDjango')
+app = Celery('GHLWebsiteDjango', broker='redis://localhost:6380/0')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
@@ -15,5 +16,11 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 @app.task(bind=True)
+def run_poll_api(self):
+    fetch_and_process_games_task()
+    pass
+
+@app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
+    pass
