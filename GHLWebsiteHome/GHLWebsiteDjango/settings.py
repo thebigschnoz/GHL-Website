3ffeed5d6b8302636292075152c28e47f9b1af1d
thebigschnoz <pendletongoodguy@gmail.com>
2025-02-24 23:15:42 -0500

    attempting to fix static

--- GHLWebsiteHome/GHLWebsiteDjango/settings.py
+++ GHLWebsiteHome/GHLWebsiteDjango/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = join(BASE_DIR, "static")
+STATIC_ROOT = join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
