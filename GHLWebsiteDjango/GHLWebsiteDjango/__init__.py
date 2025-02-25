341a41b95956a82bb31046ad914e3dc60e94c240
thebigschnoz <pendletongoodguy@gmail.com>
2025-02-24 19:35:32 -0500

    remove the import?

--- GHLWebsiteDjango/GHLWebsiteDjango/__init__.py
+++ GHLWebsiteDjango/GHLWebsiteDjango/__init__.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import, unicode_literals
-from .poll_api import fetch_and_process_games_task
 from GHLWebsiteDjango.celery import app as celery_app
 
 __all__ = ('celery_app',)
