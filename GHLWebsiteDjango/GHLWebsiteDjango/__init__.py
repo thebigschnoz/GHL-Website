ea797be4178eece2c7533d36ffd4c40a6921dbeb
thebigschnoz <pendletongoodguy@gmail.com>
2025-02-24 19:25:45 -0500

    removing prefix

--- GHLWebsiteDjango/GHLWebsiteDjango/__init__.py
+++ GHLWebsiteDjango/GHLWebsiteDjango/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import, unicode_literals
-from GHLWebsiteDjango.GHLWebsiteDjango.poll_api import fetch_and_process_games_task
+from .poll_api import fetch_and_process_games_task
 from GHLWebsiteDjango.celery import app as celery_app
 
 __all__ = ('celery_app',)
