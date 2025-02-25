68a870bef27c85d6e83a92e5b4d631176ad22bca
thebigschnoz <pendletongoodguy@gmail.com>
2025-02-24 22:46:24 -0500

    fixed syntax

--- GHLWebsiteHome/celery_app.py
+++ GHLWebsiteHome/celery_app.py
@@ -2,8 +2,8 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GHLWebsiteDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GHLWebsiteHome.GHLWebsiteDjango.settings')
 
-app = Celery('GHLWebsiteDjango', broker='redis://localhost:6379/0', include='GHLWebsiteDjango.poll_api')
+app = Celery('GHLWebsiteDjango', broker='redis://localhost:6379/0', include=['GHLWebsiteDjango.poll_api'])
 app.conf.enable_utc = False
 app.autodiscover_tasks()
