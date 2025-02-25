5ee8494d6a038d1304a316c58e3c1ea7ae59b4d7
thebigschnoz <pendletongoodguy@gmail.com>
2025-02-24 19:30:00 -0500

    fixing settings

--- GHLWebsiteDjango/celery.py
+++ GHLWebsiteDjango/celery.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GHLWebsiteDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GHLWebsiteDjango.GHLWebsiteDjango.settings')
 
 app = Celery('GHLWebsiteDjango', broker='redis://localhost:6379/0')
 app.conf.enable_utc = False
