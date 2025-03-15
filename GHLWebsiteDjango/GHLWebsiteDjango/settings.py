9e3a8cc8a88c56fde796c88aaa2312e5b1dec1f8
thebigschnoz <pendletongoodguy@gmail.com>
2025-03-14 21:15:18 -0400

    Set DEBUG to True for development environment

--- GHLWebsiteDjango/GHLWebsiteDjango/settings.py
+++ GHLWebsiteDjango/GHLWebsiteDjango/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#8uol1ab_ikdu3ufqb_)6%a+&wb@9qot6yn=8ece1ol!k$yjca'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
