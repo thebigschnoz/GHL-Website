e4c7c7b8456051c5ef7a28700d1501925e698f34
thebigschnoz <pendletongoodguy@gmail.com>
2025-03-18 22:14:39 -0400

    Update player creation logic to not add player's current team for test season games

--- GHLWebsiteDjango/GHLWebsiteApp/management/commands/poll_api.py
+++ GHLWebsiteDjango/GHLWebsiteApp/management/commands/poll_api.py
@@ -196,12 +196,21 @@ class Command(BaseCommand):
                 for team_id, team_players in game["players"].items():
                     for player_id, player_data in team_players.items():
                         players_team_instance = Team.objects.get(ea_club_num=team_id)
-                        player_instance, _ = Player.objects.get_or_create(
+                        if seasonSetting == 2:
+                            # Create player, but don't give current team
+                            player_instance, _ = Player.objects.get_or_create(
                             ea_player_num=player_id,
                             defaults={
                                 "username": player_data.get("playername", "Username Not Found"),
-                                "current_team": players_team_instance
-                        })
+                                "current_team": None
+                            })
+                        else:
+                            player_instance, _ = Player.objects.get_or_create(
+                                ea_player_num=player_id,
+                                defaults={
+                                    "username": player_data.get("playername", "Username Not Found"),
+                                    "current_team": players_team_instance
+                            })
                         pos_sorted = Position.objects.get(ea_pos=player_data.get("posSorted", 0))
                         build = player_data.get("class", 0)
                         try:
