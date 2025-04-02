a25e942c986cbc6cf69d0a423657b4ecf816277c
thebigschnoz <pendletongoodguy@gmail.com>
2025-04-01 23:55:12 -0400

    Add GoalieRecordAdmin class and update SkaterRecordAdmin to use game_num attributes

--- GHLWebsiteDjango/GHLWebsiteApp/admin.py
+++ GHLWebsiteDjango/GHLWebsiteApp/admin.py
@@ -19,7 +19,10 @@ class GameAdmin(admin.ModelAdmin):
     list_display = ("game_num", "season_num__season_text", "gamelength", "a_team_num__club_abbr", "h_team_num__club_abbr", "expected_time", "played_time", "dnf")
 
 class SkaterRecordAdmin(admin.ModelAdmin):
-    list_display = ("ea_player_num__username", "game_num", "game_num__played_time")
+    list_display = ("ea_player_num__username", "game_num__game_num", "game_num__played_time")
+
+class GoalieRecordAdmin(admin.ModelAdmin):
+    list_display = ("ea_player_num__username", "game_num__game_num", "game_num__played_time")
 
 admin.site.register(Season, SeasonsAdmin)
 admin.site.register(Game, GameAdmin)
@@ -30,7 +33,7 @@ admin.site.register(Position)
 admin.site.register(Build, BuildsAdmin)
 admin.site.register(AwardAssign)
 admin.site.register(SkaterRecord, SkaterRecordAdmin)
-admin.site.register(GoalieRecord)
+admin.site.register(GoalieRecord, GoalieRecordAdmin)
 admin.site.register(TeamRecord)
 admin.site.register(Standing)
 admin.site.register(Leader)
