285ed5876c3ce9c161611e5a516ec16cf43ab53a
thebigschnoz <pendletongoodguy@gmail.com>
2025-04-15 16:42:16 -0400

    Added form for awards dropdown - may not use

--- GHLWebsiteDjango/GHLWebsiteApp/forms.py
+++ GHLWebsiteDjango/GHLWebsiteApp/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from .models import AwardTitle
 
 class UploadFileForm(forms.Form):
     file = forms.FileField()
+
+class AwardForm(forms.Form):
+    award = forms.ModelChoiceField(queryset=AwardTitle.objects.all(), empty_label="Select an award...", label="Award")
