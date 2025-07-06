680606632ff340b9dae07188465977742ec1eb59
thebigschnoz <pendletongoodguy@gmail.com>
2025-07-06 00:48:54 -0400

    Fix AUTH_USER_MODEL with new label

--- GHLWebsiteDjango/GHLWebsiteDjango/settings.py
+++ GHLWebsiteDjango/GHLWebsiteDjango/settings.py
@@ -118,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'GHLWebsiteApp.User'
+AUTH_USER_MODEL = 'ghlwebsiteapp.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
