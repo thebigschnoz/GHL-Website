bac49e8c6c62bf8e0d5a73c8ce7bfac54faa0fc0
thebigschnoz <pendletongoodguy@gmail.com>
2025-07-06 00:46:24 -0400

    added label to app

--- GHLWebsiteDjango/GHLWebsiteApp/apps.py
+++ GHLWebsiteDjango/GHLWebsiteApp/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class GhlwebsiteappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'GHLWebsiteApp'
+    label = 'ghlwebsiteapp'
