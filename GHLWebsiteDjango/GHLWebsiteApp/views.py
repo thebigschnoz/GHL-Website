28e13a60ec857bf311a8fea481424a56063c18f6
thebigschnoz <pendletongoodguy@gmail.com>
2025-11-25 06:43:32 -0500

    Fix Season-finding bug in saving three stars

--- GHLWebsiteDjango/GHLWebsiteApp/views.py
+++ GHLWebsiteDjango/GHLWebsiteApp/views.py
@@ -2087,7 +2087,7 @@ def weekly_stats_view(request):
             return redirect(f"{reverse('weekly_stats')}?week={selected_week:%Y-%m-%d}")
 
         three_stars, created = WeeklyThreeStars.objects.update_or_create(
-            season=season_num,
+            season=get_seasonSetting(),
             week_start=selected_week,
             defaults={
                 "first_star": first_player,
